9d4e63c9cd499b3e130b252a0a093a3b4e83a227
Joshua Uwaifo <joshua@move.ai>
2021-09-09 09:14:28 +0100

    implement an autoencoder snippet using the Generative Deep Learning book, error currently occurs due to the models library not currently implemented

---
+++ research/Python/fun_generativeDeepLearning.py
@@ -0,0 +1,14 @@
+try:
+    from models.AE import Autoencoder
+    AE = Autoencoder(
+        input_dim = (28, 28, 1),
+        encoder_conv_filters = [32, 64, 64, 64],
+        encoder_conv_kernel_size= [3, 3, 3, 3],
+        encoder_conv_strides = [1, 2, 2, 1],
+        decoder_conv_t_filters = [64, 64, 32, 1],
+        decoder_conv_t_kernel_size = [3, 3, 3, 3],
+        decoder_conv_t_strides = [1, 2, 2, 1],
+        z_dim = 2
+    )
+except Exception:
+    print("Hopefully this will be figured out in due course")

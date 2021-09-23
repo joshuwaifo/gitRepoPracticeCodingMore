53626849189f2aa5f83b376dbde197e53671b0e5
Joshua Uwaifo <joshua@move.ai>
2021-09-23 09:44:53 +0100

    add work that helps compute the fundamental matrix F, using the 8/9 point algorithm found in section 5.1 on Epipolar Geometry from chapter 5 on Multiple View Geometry

--- research/Python/fun_epipolarGeometry.py
+++ research/Python/fun_epipolarGeometry.py
@@ -53,3 +53,71 @@ try:
     show()
 except Exception:
     print("Sort out the errors later")
+
+from matplotlib.pyplot import figure, show
+from mpl_toolkits.mplot3d import axes3d
+
+fig_type_Figure = figure()
+ax_type_Axes3DSubplot = fig_type_Figure.gca( projection = "3d" )
+
+# generate 3D sample data
+X_type_ndarray, Y_type_ndarray, Z_type_ndarray = axes3d.get_test_data( 0.5 )
+
+# plot the points in 3D
+ax_type_Axes3DSubplot.plot(
+    X_type_ndarray.flatten(),
+    Y_type_ndarray.flatten(),
+    Z_type_ndarray.flatten(),
+    'o'
+)
+
+show()
+
+try:
+    # plotting 3D points
+    from mpl_toolkits.mplot3d import axes3d
+    from matplotlib.pyplot import figure
+
+    fig = figure()
+    ax = fig.gca( projection = "3d" )
+    ax.plot(
+        points3D[ 0 ],
+        points3D[ 1 ],
+        points3D[ 2 ],
+        'k.'
+    )
+
+except Exception:
+    print( "Need to get the data for points3D from the first try-except block" )
+
+from numpy import zeros
+from scipy.linalg import svd
+def compute_fundamental( x1, x2 ):
+    """ Computes the fundamental matrix from corresponding points
+        (x1, x2, 3*n arrays) using the normalized 8 point algorithm.
+        each row is constructed as
+        [ x' * x, x' * y, x' * z, y' * x, y' * y, y' * z, z' * x, z' * y, z'* z ] """
+
+    n = x1.shape[ 1 ]
+    if x2.shape[ 1 ] != n:
+        raise ValueError( "Number of points don't match." )
+
+    # build matrix for equations
+    A = zeros( ( n, 9 ) )
+    for i in range( n ):
+        A[ i ] = [
+            x1[ 0, i ] * x2[ 0, i ],
+            x1[ 0, i ] * x2[ 1, i ],
+            x1[ 0, i ] * x2[ 2, i ],
+            x1[ 1, i ] * x2[ 0, i ],
+            x1[ 1, i ] * x2[ 1, i ],
+            x1[ 1, i ] * x2[ 2, i ],
+            x1[ 2, i ] * x2[ 0, i ],
+            x1[ 2, i ] * x2[ 1, i ],
+            x1[ 2, i ] * x2[ 2, i ]
+
+        ]
+
+    # compute linear least square solution
+    U, S, V = svd( A )
+    F = V[ -1 ].reshape( 3, 3)
